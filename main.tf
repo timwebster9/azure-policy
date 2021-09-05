5e2d4aed55cdced6a9ad799fc6bc5d05d74e6787
Tim Webster <tim.webster@contino.io>
2021-09-05 17:54:32 +0100

    comply to policy

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "key_vault" {
     kv_network_access_name            = "55615ac9-af46-4a59-874e-391cc3dfb490"
     policy_definition_mgmt_group_name = "parent-mgmt-group"
     policy_assignment_mgmt_group_name = "parent-mgmt-group"
-    network_acls_default_action       = "Allow"
+    network_acls_default_action       = "Deny"
 }
 
 # #####################

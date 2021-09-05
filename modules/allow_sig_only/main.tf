93cfaf332a232a2f4c116b5ba298f71f6cedfe5e
Tim Webster <tim.webster@contino.io>
2021-09-05 20:08:11 +0100

    fix

--- modules/allow_sig_only/main.tf
+++ modules/allow_sig_only/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_policy_definition" "allow_sig_only" {
 METADATA
 
   policy_rule = file("./modules/allow_sig_only/policy_defs/compute/allow_sig_only/rules.json")
-  parameters = file("./modules/allow_sig_only/policy_defs/policy_defs/compute/allow_sig_only/parameters.json")
+  parameters = file("./modules/allow_sig_only/policy_defs/compute/allow_sig_only/parameters.json")
 }
 
 resource "azurerm_management_group_policy_assignment" "allow_sig_only" {

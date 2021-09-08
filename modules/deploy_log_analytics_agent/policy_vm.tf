aeb82fad7a6183719637e96061a5968b50011a41
Tim Webster <tim.webster@contino.io>
2021-09-08 22:34:03 +0100

    re-evaluate compliance

--- modules/deploy_log_analytics_agent/policy_vm.tf
+++ modules/deploy_log_analytics_agent/policy_vm.tf
@@ -53,7 +53,8 @@ resource "azurerm_role_assignment" "role_vm" {
 }
 
 resource "azurerm_policy_remediation" "rem_vm" {
-  name                 = "linux-vm-remediation"
-  scope                = azurerm_management_group_policy_assignment.policy_assignment_vm.management_group_id
-  policy_assignment_id = azurerm_management_group_policy_assignment.policy_assignment_vm.id
+  name                    = "linux-vm-remediation"
+  resource_discovery_mode = "ReEvaluateCompliance"
+  scope                   = azurerm_management_group_policy_assignment.policy_assignment_vm.management_group_id
+  policy_assignment_id    = azurerm_management_group_policy_assignment.policy_assignment_vm.id
 }

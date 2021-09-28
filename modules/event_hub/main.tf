5bdff5228435cfa0a7a3e398af5cf4ded1d9a388
Tim Webster <tim.webster@contino.io>
2021-09-28 16:33:44 +0100

    add new policy

--- modules/event_hub/main.tf
+++ modules/event_hub/main.tf
@@ -1,3 +1,4 @@
+### Deny public network access
 resource "azurerm_policy_definition" "event_hub_deny_public_access" {
   name                  = "eh-network-access"
   policy_type           = "Custom"
@@ -38,6 +39,47 @@ METADATA
 PARAMETERS
 }
 
+# Must be zone-redundant
+resource "azurerm_policy_definition" "event_hub_zone_redundant" {
+  name                  = "eh-zone-redundant"
+  policy_type           = "Custom"
+  mode                  = "Indexed"
+  display_name          = "Event Hubs should be zone-redundant"
+  management_group_name = data.azurerm_management_group.policy_definition_mgmt_group.name
+
+  metadata = <<METADATA
+    {
+    "category": "Event Hub"
+    }
+
+METADATA
+
+  policy_rule = file("${path.module}/policy_defs/eventhub_zone_redundant/rules.json")
+  parameters = file("${path.module}/policy_defs/eventhub_zone_redundant/parameters.json")
+}
+
+resource "azurerm_management_group_policy_assignment" "event_hub_zone_redundant" {
+  name                 = "eh-zone-redundant"
+  policy_definition_id = azurerm_policy_definition.event_hub_zone_redundant.id
+  management_group_id  = data.azurerm_management_group.policy_assignment_mgmt_group.id
+  description          = "Policy Assignment test"
+  display_name         = "Event Hubs should be zone-redundant"
+
+  metadata = <<METADATA
+    {
+    "category": "Event Hub"
+    }
+METADATA
+
+  parameters = <<PARAMETERS
+{
+  "effect": {
+    "value": "Deny"
+  }
+}
+PARAMETERS
+}
+
 resource "azurerm_resource_group" "policy_rg" {
   name     = var.resource_group_name
   location = var.location
@@ -60,6 +102,7 @@ resource "azurerm_eventhub_namespace" "example" {
   }
 
   depends_on = [
-    azurerm_management_group_policy_assignment.event_hub_deny_public_access
+    azurerm_management_group_policy_assignment.event_hub_deny_public_access,
+    azurerm_management_group_policy_assignment.event_hub_zone_redundant
   ]
 }

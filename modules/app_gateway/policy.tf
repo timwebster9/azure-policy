8c337645f56e178368864c4629c21da7420f6706
Tim Webster <tim.webster@contino.io>
2021-09-30 15:56:54 +0100

    add https listener policy

--- modules/app_gateway/policy.tf
+++ modules/app_gateway/policy.tf
@@ -73,3 +73,38 @@ resource "azurerm_management_group_policy_assignment" "appgateway_frontend_ports
 }
 PARAMETERS
 }
+
+# HTTPS-only listeners
+resource "azurerm_policy_definition" "appgateway_listener_https_only" {
+  name                  = "appgateway-listener"
+  policy_type           = "Custom"
+  mode                  = "Indexed"
+  display_name          = "Application Gateway listeners should use HTTPS only"
+  management_group_name = data.azurerm_management_group.policy_definition_mgmt_group.name
+
+  metadata = <<METADATA
+    {
+    "category": "Network"
+    }
+
+METADATA
+
+  policy_rule = file("${path.module}/policy_defs/listener_https_only/rules.json")
+  parameters = file("${path.module}/policy_defs/listener_https_only/parameters.json")
+}
+
+resource "azurerm_management_group_policy_assignment" "appgateway_listener_https_only" {
+  name                 = azurerm_policy_definition.appgateway_listener_https_only.name
+  policy_definition_id = azurerm_policy_definition.appgateway_listener_https_only.id
+  management_group_id  = data.azurerm_management_group.policy_assignment_mgmt_group.id
+  description          = "Policy Assignment test"
+  display_name         = azurerm_policy_definition.appgateway_listener_https_only.display_name
+
+  parameters = <<PARAMETERS
+{
+  "effect": {
+    "value": "Deny"
+  }
+}
+PARAMETERS
+}

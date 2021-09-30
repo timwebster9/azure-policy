e14fffe4a21ad4768e1abd7db47951c29cc89ffc
Tim Webster <tim.webster@contino.io>
2021-09-30 15:08:13 +0100

    fix

--- modules/app_gateway/appgateway.tf
+++ modules/app_gateway/appgateway.tf
@@ -28,7 +28,7 @@ resource "azurerm_public_ip" "example" {
   name                = "example-pip"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 

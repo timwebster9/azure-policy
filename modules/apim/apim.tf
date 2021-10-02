067e7330bd3daee5d48c2aefb44bdfe759399227
Tim Webster <tim.webster@contino.io>
2021-10-02 18:12:59 +0100

    add KV, cert and custom domain

--- modules/apim/apim.tf
+++ modules/apim/apim.tf
@@ -17,6 +17,152 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_key_vault" "example" {
+  name                        = "apimkv675674475675"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = false
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+  //enable_rbac_authorization   = true
+
+ access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_api_management.example.identity.principal_id
+
+    certificate_permissions = [
+      "create",
+      "delete",
+      "deleteissuers",
+      "get",
+      "getissuers",
+      "import",
+      "list",
+      "listissuers",
+      "managecontacts",
+      "manageissuers",
+      "setissuers",
+      "update",
+    ]
+
+    key_permissions = [
+      "backup",
+      "create",
+      "decrypt",
+      "delete",
+      "encrypt",
+      "get",
+      "import",
+      "list",
+      "purge",
+      "recover",
+      "restore",
+      "sign",
+      "unwrapKey",
+      "update",
+      "verify",
+      "wrapKey",
+    ]
+
+    secret_permissions = [
+      "backup",
+      "delete",
+      "get",
+      "list",
+      "purge",
+      "recover",
+      "restore",
+      "set",
+    ]
+  }
+
+ access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    certificate_permissions = [
+      "create",
+      "delete",
+      "deleteissuers",
+      "get",
+      "getissuers",
+      "import",
+      "list",
+      "listissuers",
+      "managecontacts",
+      "manageissuers",
+      "setissuers",
+      "update",
+    ]
+
+    key_permissions = [
+      "backup",
+      "create",
+      "decrypt",
+      "delete",
+      "encrypt",
+      "get",
+      "import",
+      "list",
+      "purge",
+      "recover",
+      "restore",
+      "sign",
+      "unwrapKey",
+      "update",
+      "verify",
+      "wrapKey",
+    ]
+
+    secret_permissions = [
+      "backup",
+      "delete",
+      "get",
+      "list",
+      "purge",
+      "recover",
+      "restore",
+      "set",
+    ]
+  }
+}
+
+resource "azurerm_key_vault_certificate" "example" {
+  name         = "apim-cert"
+  key_vault_id = azurerm_key_vault.example.id
+
+  certificate {
+    contents = filebase64("${path.module}/cert.pfx")
+    password = ""
+  }
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = false
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+  }
+}
+
+# resource "azurerm_user_assigned_identity" "apim_uami" {
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+
+#   name = "apim-uami"
+# }
+
 resource "azurerm_api_management" "example" {
   name                = "twpolicyapim8979uaaf"
   location            = azurerm_resource_group.example.location
@@ -31,6 +177,17 @@ resource "azurerm_api_management" "example" {
 
   virtual_network_configuration {
     subnet_id = azurerm_subnet.example.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  hostname_configuration {
+    proxy {
+      host_name = "api.deggymacets.com"
+      keu_vault_id = azurerm_key_vault.example.id
+    }
   }
 
   depends_on = [

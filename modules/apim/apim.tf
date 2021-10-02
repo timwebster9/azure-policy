d75d9a1e158ffd8891b9ab74b1b3882abe34e337
Tim Webster <tim.webster@contino.io>
2021-10-02 18:49:04 +0100

    fix

--- modules/apim/apim.tf
+++ modules/apim/apim.tf
@@ -89,7 +89,7 @@ resource "azurerm_key_vault" "example" {
 resource "azurerm_key_vault_access_policy" "apim_msi" {
   key_vault_id = azurerm_key_vault.example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_api_management.example.identity.principal_id
+  object_id    = azurerm_api_management.example.identity[0].principal_id
 
   secret_permissions = [
     "Get", "List"

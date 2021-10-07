f76006f0682f387cb6f016b920e02e9d5fe393d8
Tim Webster <tim.webster@contino.io>
2021-10-07 23:28:37 +0100

    fix

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_mssql_database" "test" {
   license_type   = "LicenseIncluded"
   max_size_gb    = 4
   read_scale     = true
-  sku_name       = "Basic"
+  sku_name       = "GP_S_Gen5_1"
   zone_redundant = true
 
   # extended_auditing_policy {

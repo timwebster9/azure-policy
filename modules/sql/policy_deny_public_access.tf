72802f4a9551c37b03335f6806a47be5b605bd20
Tim Webster <tim.webster@contino.io>
2021-10-07 22:33:55 +0100

    fix

--- modules/sql/policy_deny_public_access.tf
+++ modules/sql/policy_deny_public_access.tf
@@ -1,9 +1,9 @@
 resource "azurerm_management_group_policy_assignment" "deny_public_access" {
   name                 = "sql_deny_public_access"
-  policy_definition_id = data.azurerm_policy_definition.azurerm_policy_definition.id
+  policy_definition_id = data.azurerm_policy_definition.sql_deny_public_access.id
   management_group_id  = data.azurerm_management_group.policy_assignment_mgmt_group.id
   description          = "Policy Assignment test"
-  display_name         = data.azurerm_policy_definition.azurerm_policy_definition.display_name
+  display_name         = data.azurerm_policy_definition.sql_deny_public_access.display_name
 
   parameters = <<PARAMETERS
 {

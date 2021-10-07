64516d7b3f027b400bce770fbd56606f89bfc192
Tim Webster <tim.webster@contino.io>
2021-10-07 23:01:33 +0100

    fix

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_mssql_server" "example" {
-  name                         = "mssqlserver"
+  name                         = "timwpolicysql098as0fsd"
   resource_group_name          = azurerm_resource_group.example.name
   location                     = azurerm_resource_group.example.location
   version                      = "12.0"

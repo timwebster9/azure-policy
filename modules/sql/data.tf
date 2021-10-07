3fb53827580a4d59f5d98239a4ad49dc8027b3ae
Tim Webster <tim.webster@contino.io>
2021-10-07 22:29:50 +0100

    sql policy public access

--- modules/sql/data.tf
+++ modules/sql/data.tf
@@ -7,3 +7,7 @@ data "azurerm_management_group" "policy_definition_mgmt_group" {
 data "azurerm_management_group" "policy_assignment_mgmt_group" {
   name = var.policy_assignment_mgmt_group_name
 }
+
+data "azurerm_policy_definition" "sql_deny_public_access" {
+  name = "1b8ca024-1d5c-4dec-8995-b1a932b41780"
+}
---
+++ modules/sql/policy_deny_public_access.tf
@@ -0,0 +1,18 @@
+resource "azurerm_management_group_policy_assignment" "deny_public_access" {
+  name                 = "sql_deny_public_access"
+  policy_definition_id = data.azurerm_policy_definition.azurerm_policy_definition.id
+  management_group_id  = data.azurerm_management_group.policy_assignment_mgmt_group.id
+  description          = "Policy Assignment test"
+  display_name         = data.azurerm_policy_definition.azurerm_policy_definition.display_name
+
+  parameters = <<PARAMETERS
+{
+  "effect": {
+    "value": "Deny"
+  },
+  "version": {
+    "value": "deny-inbound-pres-tcp"
+  }
+}
+PARAMETERS
+} 

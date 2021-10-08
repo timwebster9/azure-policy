b20bb773ccbe2c9242d4b904196a57f0d66e6790
Tim Webster <tim.webster@contino.io>
2021-10-08 08:27:51 +0100

    fix

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_log_analytics_workspace" "example" {
   name                = "policytestworkspace"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  sku                 = "Free"
+  sku                 = "PerGB2018"
   retention_in_days   = 7
 }
 

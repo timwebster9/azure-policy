4bf5f2b94d55efc6d7f0873b2f4d0bf99765bc8f
Tim Webster <tim.webster@contino.io>
2021-10-09 13:38:08 +0100

    update name

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -56,7 +56,7 @@ resource "azurerm_eventhub_namespace_authorization_rule" "namespace_rule" {
 }
 
 resource "azurerm_mssql_server" "example" {
-  name                         = "timwpolicysql098as0fsd"
+  name                         = "timw9078safsaf"
   resource_group_name          = azurerm_resource_group.example.name
   location                     = azurerm_resource_group.example.location
   version                      = "12.0"

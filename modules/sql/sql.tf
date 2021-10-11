b41ea5318400bcba2b7da35609ff1cd1651f7ec2
Tim Webster <tim.webster@contino.io>
2021-10-11 11:12:11 +0100

    fix

--- modules/sql/sql.tf
+++ modules/sql/sql.tf
@@ -52,7 +52,7 @@ resource "azurerm_mssql_database" "test" {
   server_id      = azurerm_mssql_server.example.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
+  #max_size_gb    = 4
   #read_scale     = true
   sku_name       = "S0"
   zone_redundant = true

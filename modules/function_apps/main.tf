f8025318f7d659092a192758c29c8316e505e486
Tim Webster <tim.webster@contino.io>
2021-10-12 17:21:42 +0100

    fix

--- modules/function_apps/main.tf
+++ modules/function_apps/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_function_app" "example" {
-  name                       = "test-azure-functions"
+  name                       = "timwpolicyfunction908sfsd"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
   app_service_plan_id        = azurerm_app_service_plan.example.id

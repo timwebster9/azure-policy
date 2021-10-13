d70645697517db2bf4f8c6c32f309b38c0916825
Tim Webster <tim.webster@contino.io>
2021-10-13 16:29:38 +0100

    udpate

--- modules/function_apps/main.tf
+++ modules/function_apps/main.tf
@@ -15,6 +15,7 @@ resource "azurerm_app_service_plan" "example" {
   name                = "azure-functions-test-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  zone_redundant      = true
 
   sku {
     tier = "Standard"

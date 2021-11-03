4db643dddcc5e88979cffd3b52654d32899b72e1
Tim Webster <tim.webster@contino.io>
2021-11-03 21:15:09 +0000

    fix

--- modules/cosmos_db/main.tf
+++ modules/cosmos_db/main.tf
@@ -9,6 +9,7 @@ resource "azurerm_cosmosdb_account" "db" {
   resource_group_name = azurerm_resource_group.example.name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
+  public_network_access_enabled = true
 
   #enable_automatic_failover = true
 
@@ -19,7 +20,7 @@ resource "azurerm_cosmosdb_account" "db" {
   }
 
   geo_location {
-    location          = var.failover_location
+    location          = "ukwest"
     failover_priority = 1
   }
 

3f8b16e660e08e7b57a2baf7cd90cdf060924e8b
Tim Webster <tim.webster@contino.io>
2021-11-05 09:22:22 +0000

    revert

--- modules/cosmos_db/main.tf
+++ modules/cosmos_db/main.tf
@@ -13,16 +13,16 @@ resource "azurerm_cosmosdb_account" "db" {
 
   #enable_automatic_failover = true
 
-  # consistency_policy {
-  #   consistency_level       = "BoundedStaleness"
-  #   max_interval_in_seconds = 10
-  #   max_staleness_prefix    = 200
-  # }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
 
-  # geo_location {
-  #   location          = azurerm_resource_group.example.location
-  #   failover_priority = 0
-  # }
+  geo_location {
+    location          = azurerm_resource_group.example.location
+    failover_priority = 0
+  }
 
   depends_on = [
     azurerm_management_group_policy_assignment.disable_public_network_access,

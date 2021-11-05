111011a196025d07076781c8de250abdafc9f7d1
Tim Webster <tim.webster@contino.io>
2021-11-05 15:34:54 +0000

    add rules

--- modules/firewall_policy/main.tf
+++ modules/firewall_policy/main.tf
@@ -10,3 +10,28 @@ resource "azurerm_firewall_policy" "example" {
   location            = azurerm_resource_group.example.location
   sku                 = "Premium"
 }
+
+resource "azurerm_firewall_policy_rule_collection_group" "example" {
+  name               = "example-fwpolicy-rcg"
+  firewall_policy_id = azurerm_firewall_policy.example.id
+  priority           = 500
+  
+  application_rule_collection {
+    name     = "app_rule_collection1"
+    priority = 500
+    action   = "Deny"
+    rule {
+      name = "app_rule_collection1_rule1"
+      protocols {
+        type = "Http"
+        port = 80
+      }
+      protocols {
+        type = "Https"
+        port = 443
+      }
+      source_addresses  = ["10.0.0.1"]
+      destination_fqdns = [".microsoft.com"]
+    }
+  }
+}

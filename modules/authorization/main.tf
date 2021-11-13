81a8cfe0b146ac825307510946e76eed262d85df
Tim Webster <tim.webster@contino.io>
2021-11-13 14:45:28 +0000

    fix

--- modules/authorization/main.tf
+++ modules/authorization/main.tf
@@ -5,13 +5,13 @@ resource "azurerm_resource_group" "example" {
 
 resource "azuread_application" "example" {
   display_name = "example"
-  owners       = [data.azuread_client_config.current.object_id]
+  owners       = [data.azurerm_client_config.current.object_id]
 }
 
 resource "azuread_service_principal" "example" {
   application_id               = azuread_application.example.application_id
   app_role_assignment_required = false
-  owners                       = [data.azuread_client_config.current.object_id]
+  owners                       = [data.azurerm_client_config.current.object_id]
 }
 
 resource "azurerm_role_assignment" "example" {

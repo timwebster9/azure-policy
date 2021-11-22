17ab9f0a1f491eaab6e1bf45397f484555635f34
Tim Webster <tim.webster@contino.io>
2021-11-22 23:17:30 +0000

    fix

--- modules/sql/monitoring.tf
+++ modules/sql/monitoring.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "secondary_rg" {
 resource "azurerm_storage_account" "secondary_sa" {
   provider                  = azurerm.secondary
 
-  name                      = "timwpolicydiags908745"
+  name                      = "timwsecdiags2535"
   resource_group_name       = azurerm_resource_group.secondary_rg.name
   location                  = azurerm_resource_group.secondary_rg.location
   account_tier              = "Standard"

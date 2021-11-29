5a2c0e43221515f06f0a0fdc4ebfd27d7458b2a6
Tim Webster <tim.webster@contino.io>
2021-11-29 13:29:23 +0000

    add non-TLS 1.2 function

--- modules/function_apps/main.tf
+++ modules/function_apps/main.tf
@@ -200,3 +200,49 @@ resource "azurerm_function_app" "example" {
 #   app_service_id = azurerm_function_app.example.id
 #   subnet_id      = azurerm_subnet.example.id
 # }
+
+resource "azurerm_function_app" "tls_11" {
+  name                       = "timwpolicyfunctionTLS11"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.ep.id
+  storage_account_name       = azurerm_storage_account.function.name
+  storage_account_access_key = azurerm_storage_account.function.primary_access_key
+  https_only                 = true  # policy check
+  version                    = "~3"  # policy check
+  
+  site_config {
+    min_tls_version        = "1.0" # policy check
+    vnet_route_all_enabled = true  # policy check
+    elastic_instance_minimum = 3
+
+    # ip_restriction =  [
+    #   {
+    #     action = "Allow"
+    #     ip_address = "212.159.71.60/32"
+    #     name = "test"
+    #     priority = 100
+    #     headers = null
+    #     service_tag = null
+    #     virtual_network_subnet_id = null
+    #   }
+    # ]
+  }
+
+  depends_on = [
+    azurerm_policy_definition.app_service_https_only,
+    azurerm_policy_definition.app_service_tls_version,
+    azurerm_policy_definition.app_service_vnet_route_all,
+    azurerm_policy_definition.function_diagnostics,
+    azurerm_policy_definition.app_service_ip_restrictions,
+    azurerm_management_group_policy_assignment.function_runtime_version,
+    azurerm_management_group_policy_assignment.app_service_https_only,
+    azurerm_management_group_policy_assignment.app_service_tls_version,
+    azurerm_management_group_policy_assignment.app_service_vnet_route_all,
+    azurerm_management_group_policy_assignment.disable_public_network_access,
+    azurerm_management_group_policy_assignment.function_runtime_version,
+    azurerm_management_group_policy_assignment.function_diagnostics,
+    azurerm_management_group_policy_assignment.app_service_ip_restrictions,
+    azurerm_management_group_policy_assignment.vnet_injection
+  ]
+}

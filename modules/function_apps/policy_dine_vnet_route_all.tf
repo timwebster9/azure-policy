51702a183f78a8b50b5e4105618d738f62b387d4
Tim Webster <tim.webster@contino.io>
2021-12-02 10:38:13 +0000

    fix

--- modules/function_apps/policy_dine_vnet_route_all.tf
+++ modules/function_apps/policy_dine_vnet_route_all.tf
@@ -4,11 +4,6 @@ resource "azurerm_policy_definition" "dine_vnet_route_all" {
   mode                  = "Indexed"
   display_name          = "App Service apps should enable outbound non-RFC 1918 traffic to Azure Virtual Network (custom)"
   management_group_name = data.azurerm_management_group.policy_definition_mgmt_group.name
-  location             = var.location
-  
-  identity {
-    type = "SystemAssigned"
-  }
 
   metadata = <<METADATA
     {
@@ -27,7 +22,12 @@ resource "azurerm_management_group_policy_assignment" "dine_vnet_route_all" {
   management_group_id  = data.azurerm_management_group.policy_assignment_mgmt_group.id
   description          = "Policy Assignment test"
   display_name         = azurerm_policy_definition.dine_vnet_route_all.display_name
-
+  location             = var.location
+  
+  identity {
+    type = "SystemAssigned"
+  }
+  
   parameters = <<PARAMETERS
 {
   "effect": {

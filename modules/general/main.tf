6d7c51a4a52c85ca56a43ab6329989f43c185f0d
Tim Webster <tim.webster@contino.io>
2022-01-19 14:10:42 +0000

    fix

--- modules/general/main.tf
+++ modules/general/main.tf
@@ -15,22 +15,24 @@ resource "azurerm_data_factory" "example" {
 
 }
 
-data "azurerm_storage_account" "example" {
-  name                = "adfsa09890809"
-  resource_group_name = azurerm_resource_group.example.name
-
+resource "azurerm_storage_account" "example" {
+  name                     = "adfsa098908908"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
   depends_on = [
     azurerm_policy_definition.whitelist_regions,
     azurerm_management_group_policy_assignment.whitelist_regions,
   ]
-
 }
 
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "example" {
   name                = "adfsalinkedservice"
   resource_group_name = azurerm_resource_group.example.name
   data_factory_id     = azurerm_data_factory.example.id
-  connection_string   = data.azurerm_storage_account.example.primary_connection_string
+  connection_string   = azurerm_storage_account.example.primary_connection_string
 
   depends_on = [
     azurerm_policy_definition.whitelist_regions,

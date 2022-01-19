5983d6532bc021583a0e575272b9792252c4da27
Tim Webster <tim.webster@contino.io>
2022-01-19 12:52:10 +0000

    update

--- modules/general/main.tf
+++ modules/general/main.tf
@@ -7,11 +7,23 @@ resource "azurerm_data_factory" "example" {
   name                = "example"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+
+  depends_on = [
+    azurerm_policy_definition.whitelist_regions,
+    azurerm_management_group_policy_assignment.whitelist_regions,
+  ]
+  
 }
 
 data "azurerm_storage_account" "example" {
   name                = "adfsa09890809"
   resource_group_name = azurerm_resource_group.example.name
+
+  depends_on = [
+    azurerm_policy_definition.whitelist_regions,
+    azurerm_management_group_policy_assignment.whitelist_regions,
+  ]
+
 }
 
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "example" {
@@ -19,6 +31,12 @@ resource "azurerm_data_factory_linked_service_azure_blob_storage" "example" {
   resource_group_name = azurerm_resource_group.example.name
   data_factory_id     = azurerm_data_factory.example.id
   connection_string   = data.azurerm_storage_account.example.primary_connection_string
+
+  depends_on = [
+    azurerm_policy_definition.whitelist_regions,
+    azurerm_management_group_policy_assignment.whitelist_regions,
+  ]
+
 }
 
 resource "azurerm_data_factory_dataset_azure_blob" "example" {
@@ -29,4 +47,10 @@ resource "azurerm_data_factory_dataset_azure_blob" "example" {
 
   path     = "foo"
   filename = "bar.png"
+
+  depends_on = [
+    azurerm_policy_definition.whitelist_regions,
+    azurerm_management_group_policy_assignment.whitelist_regions,
+  ]
+
 }

deefd1a03748735c17a4162c83d6f34f51b2bba6
Tim Webster <tim.webster@contino.io>
2022-01-19 20:02:45 +0000

    update

--- modules/general/main.tf
+++ modules/general/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  
+
   depends_on = [
     azurerm_policy_definition.whitelist_regions,
     azurerm_management_group_policy_assignment.whitelist_regions,
@@ -55,4 +55,27 @@ resource "azurerm_data_factory_dataset_azure_blob" "example" {
     azurerm_management_group_policy_assignment.whitelist_regions,
   ]
 
+}
+
+data "azurerm_cosmosdb_account" "example" {
+  name                = "adfcosmos098098098"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_data_factory_linked_service_cosmosdb" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  data_factory_id     = azurerm_data_factory.example.id
+  account_endpoint    = azurerm_cosmosdb_account.example.endpoint
+  account_key         = data.azurerm_cosmosdb_account.example.primary_access_key
+  database            = "foo"
+}
+
+resource "azurerm_data_factory_dataset_cosmosdb_sqlapi" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  data_factory_id     = azurerm_data_factory.example.id
+  linked_service_name = azurerm_data_factory_linked_service_cosmosdb.example.name
+
+  collection_name = "bar"
 }

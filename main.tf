9459505f48e4f958d73e90a24bc5c17b64f2a141
Tim Webster <tim.webster@contino.io>
2022-01-28 14:16:06 +0000

    cosmos

--- main.tf
+++ main.tf
@@ -116,15 +116,15 @@
 #     policy_assignment_mgmt_group_name = "parent-mgmt-group"
 # }
 
-module "sql" {
-    source = "./modules/sql"
+# module "sql" {
+#     source = "./modules/sql"
 
-    location                          = var.location
-    resource_group_name               = "policy-rg"
+#     location                          = var.location
+#     resource_group_name               = "policy-rg"
 
-    policy_definition_mgmt_group_name = "parent-mgmt-group"
-    policy_assignment_mgmt_group_name = "parent-mgmt-group"
-}
+#     policy_definition_mgmt_group_name = "parent-mgmt-group"
+#     policy_assignment_mgmt_group_name = "parent-mgmt-group"
+# }
 
 # module "functions" {
 #     source = "./modules/function_apps"
@@ -146,15 +146,15 @@ module "sql" {
 #     policy_assignment_mgmt_group_name = "parent-mgmt-group"
 # }
 
-# module "cosmos_db" {
-#     source = "./modules/cosmos_db"
+module "cosmos_db" {
+    source = "./modules/cosmos_db"
 
-#     location                          = var.location
-#     resource_group_name               = "policy-rg"
+    location                          = var.location
+    resource_group_name               = "policy-rg"
 
-#     policy_definition_mgmt_group_name = "parent-mgmt-group"
-#     policy_assignment_mgmt_group_name = "parent-mgmt-group"
-# }
+    policy_definition_mgmt_group_name = "parent-mgmt-group"
+    policy_assignment_mgmt_group_name = "parent-mgmt-group"
+}
 
 # module "firewall_policy" {
 #     source = "./modules/firewall_policy"

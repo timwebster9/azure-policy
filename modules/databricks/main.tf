5bd9f93dd04dab72bc109453b11b0d6eaf3d4224
Tim Webster <tim.webster@contino.io>
2022-02-01 18:54:34 +0000

    fix

--- modules/databricks/main.tf
+++ modules/databricks/main.tf
@@ -8,17 +8,6 @@ resource "azurerm_virtual_network" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
-
-  subnet {
-    name           = "public-sn"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "private-sn"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
-  }
 }
 
 resource "azurerm_subnet" "public" {

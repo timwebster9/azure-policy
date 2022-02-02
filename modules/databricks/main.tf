ff3a74fe42cdfb354dec971c011374f91bdfa9ed
Tim Webster <tim.webster@contino.io>
2022-02-02 18:02:12 +0000

    fix

--- modules/databricks/main.tf
+++ modules/databricks/main.tf
@@ -115,7 +115,7 @@ resource "azurerm_databricks_workspace" "fail_no_vnet_injection" {
   depends_on = [
     azurerm_policy_definition.scc,
     azurerm_policy_definition.vnet_injection,
-    azurerm_management_group_policy_assignment.scc,
+    #azurerm_management_group_policy_assignment.scc,
     azurerm_management_group_policy_assignment.vnet_injection
   ]
 }
@@ -142,6 +142,6 @@ resource "azurerm_databricks_workspace" "pass_scc" {
 
   depends_on = [
     azurerm_policy_definition.scc,
-    azurerm_management_group_policy_assignment.scc
+    #azurerm_management_group_policy_assignment.scc
   ]
 }

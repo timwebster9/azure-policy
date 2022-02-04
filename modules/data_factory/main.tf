b12b7163b8dcdb593deadc9ea8acd9edab679bdb
Tim Webster <tim.webster@contino.io>
2022-02-04 00:43:01 +0000

    fix

--- modules/data_factory/main.tf
+++ modules/data_factory/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_data_factory" "example" {
-  name                = "example"
+  name                = "asdfd89asf7sd89f"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 

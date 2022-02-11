f8df2c30d68b25389ab6e25e38e6c3626b6fc3c0
Tim Webster <tim.webster@contino.io>
2022-02-11 11:27:29 +0000

    fix

--- modules/machine_learning/main.tf
+++ modules/machine_learning/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "example" {
 }
 
 resource "azurerm_subnet" "aks" {
-  name                 = "example-subnet"
+  name                 = "aks-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.2.0.0/24"]

e59b1bb62741f88c86e5282a45740b64880e6a09
Tim Webster <tim.webster@contino.io>
2022-04-26 09:28:56 +0100

    event grid system topics

--- modules/event_grid/main.tf
+++ modules/event_grid/main.tf
@@ -66,3 +66,30 @@ resource "azurerm_eventgrid_topic" "examplepass" {
     azurerm_management_group_policy_assignment.topic_disable_local_auth
   ]
 }
+
+# System topic
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_eventgrid_system_topic" "example" {
+  name                   = "example-topic"
+  resource_group_name    = azurerm_resource_group.example.name
+  location               = azurerm_resource_group.example.location
+  source_arm_resource_id = azurerm_storage_account.example.id
+  topic_type             = "Microsoft.Storage.StorageAccounts"
+
+  depends_on = [
+    azurerm_policy_definition.system_topic_diagnostics,
+    azurerm_management_group_policy_assignment.system_topic_diagnostics
+  ]
+
+}
---
+++ modules/event_grid/policy_eg_system_topic_diagnostics.tf
@@ -0,0 +1,65 @@
+resource "azurerm_policy_definition" "system_topic_diagnostics" {
+  name                  = "system_topic_diagnostics"
+  policy_type           = "Custom"
+  mode                  = "All"
+  display_name          = "Deploy Event Grid System Topic Diagnostics"
+  management_group_name = data.azurerm_management_group.policy_definition_mgmt_group.name
+
+  metadata = <<METADATA
+    {
+    "category": "Event Grid"
+    }
+
+METADATA
+
+  policy_rule = file("${path.module}/policy_defs/eg_system_topic_diagnostics/rules.json")
+  parameters = file("${path.module}/policy_defs/eg_system_topic_diagnostics/parameters.json")
+}
+
+resource "azurerm_management_group_policy_assignment" "system_topic_diagnostics" {
+  name                 = "system_topic_diagnostics"
+  policy_definition_id = azurerm_policy_definition.system_topic_diagnostics.id
+  management_group_id  = data.azurerm_management_group.policy_assignment_mgmt_group.id
+  description          = "Policy Assignment test"
+  display_name         = azurerm_policy_definition.system_topic_diagnostics.display_name
+  location             = var.location
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  parameters = <<PARAMETERS
+{
+  "effect": {
+    "value": "DeployIfNotExists"
+  },
+  "eventHubRuleId": {
+    "value": "${azurerm_eventhub_namespace_authorization_rule.namespace_rule.id}"
+  },
+  "eventHubName": {
+    "value": "${azurerm_eventhub.logs.name}"
+  },
+  "logAnalyticsWorkspaceId": {
+    "value": "${azurerm_log_analytics_workspace.example.id}"
+  },
+  "storageAccountId": {
+    "value": "${azurerm_storage_account.diagsstorageaccount.id}"
+  },
+  "storageRetentionDays": {
+    "value": 365
+  },
+  "metricsEnabled": {
+    "value": "True"
+  },
+  "logsEnabled": {
+    "value": "True"
+  }
+}
+PARAMETERS
+}
+
+resource "azurerm_role_assignment" "system_topic_diagnostics" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_management_group_policy_assignment.system_topic_diagnostics.identity[0].principal_id
+}

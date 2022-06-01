b6dc9472718b5571ddb6128a08c73351aa5d83e4
Tim Webster <tim.webster@contino.io>
2022-06-01 15:59:32 +0100

    add fail test case

--- modules/naming/main.tf
+++ modules/naming/main.tf
@@ -3,8 +3,21 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
+# should pass, follows naming convention
 resource "azurerm_virtual_network" "example" {
   name                = "rlg-npd-pol-uks-vnet-policytest"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+
+  depends_on = [
+    azurerm_policy_definition.naming_convention,
+    azurerm_management_group_policy_assignment.naming_convention
+  ]
+}
+
+resource "azurerm_virtual_network" "examplefail" {
+  name                = "rlg-prod-pol-uks-vnet-policytest"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
@@ -18,7 +31,7 @@ resource "azurerm_virtual_network" "example" {
 resource "azurerm_resource_group" "testrg" {
   name     = "testrg"
   location = var.location
-  
+
   depends_on = [
     azurerm_policy_definition.naming_convention,
     azurerm_management_group_policy_assignment.naming_convention

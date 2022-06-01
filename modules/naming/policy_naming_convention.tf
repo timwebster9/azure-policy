cd57321bce43e5ad09c6fa0f6e1e51f366f7522c
Tim Webster <tim.webster@contino.io>
2022-06-01 14:53:34 +0100

    fixes

--- modules/naming/policy_naming_convention.tf
+++ modules/naming/policy_naming_convention.tf
@@ -3,7 +3,7 @@ resource "azurerm_policy_definition" "naming_convention" {
   policy_type           = "Custom"
   mode                  = "Indexed"
   display_name          = "Resources should use the provided naming convention"
-  management_group_name = data.azurerm_management_group.policy_definition_mgmt_group.name
+  management_group_id   = data.azurerm_management_group.policy_definition_mgmt_group.id
 
   metadata = <<METADATA
     {
--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azurerm = {
       source = "azurerm"
-      version = "3.2.0"
+      version = "3.8.0"
     }
   }
   backend "azurerm" {

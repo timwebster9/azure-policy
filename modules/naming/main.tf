1cc13c8f4bf4f7db7c95c2a15686dc7aa6fe1401
Tim Webster <tim.webster@contino.io>
2022-06-01 15:16:30 +0100

    update

--- modules/naming/main.tf
+++ modules/naming/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "virtualNetwork1"
+  name                = "rlg-dev-pol-uks-vnet"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]

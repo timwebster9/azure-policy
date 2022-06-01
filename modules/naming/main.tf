26c62e8b06c679a5d332c654dc8052fb1be9762b
Tim Webster <tim.webster@contino.io>
2022-06-01 15:32:43 +0100

    update

--- modules/naming/main.tf
+++ modules/naming/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "rlg-npd-pol-uks-vnet"
+  name                = "rlg-npd-pol-uks-vnet-policytest"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
